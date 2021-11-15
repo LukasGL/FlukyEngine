df0298001c5ea4a17ab00731a4a5f6ed812bb6af
LukasGL <51293435+LukasGL@users.noreply.github.com>
2021-11-15 01:29:41 -0300

    Added second player to ex_players

--- examples-fluky/ex_players.cpp
+++ examples-fluky/ex_players.cpp
@@ -11,9 +11,17 @@ public:
 		auto& boxComponent = cube.AddComponent<Fluky::BoxComponent>();
 		auto& transformComponent = cube.AddComponent<Fluky::TransformComponent>();
 		transformComponent.RotateXYZ(0.f, 0.f, 0.f);
-		transformComponent.SetTranslation(0.f, 0.f, 0.f);
+		transformComponent.SetTranslation(1.5f, 0.f, 0.f);
 		auto& playerComponent = cube.AddComponent<Fluky::PlayerComponent>();
 		playerComponent.SetPlayerId(&world.GetJoystickInput(), 0);
+
+		auto cube2 = world.CreateGameObject();
+		auto& boxComponent2 = cube2.AddComponent<Fluky::BoxComponent>();
+		auto& transformComponent2 = cube2.AddComponent<Fluky::TransformComponent>();
+		transformComponent2.RotateXYZ(0.f, 0.f, 0.f);
+		transformComponent2.SetTranslation(-1.5f, 0.f, 0.f);
+		auto& playerComponent2 = cube2.AddComponent<Fluky::PlayerComponent>();
+		playerComponent2.SetPlayerId(&world.GetJoystickInput(), 1);
 
 	}
 
@@ -25,9 +33,17 @@ public:
 		for (int i = 0; i < gameObjects.size(); i++) {
 			auto& player = gameObjects.at(i).GetComponent<Fluky::PlayerComponent>();
 			auto& transf = gameObjects.at(i).GetComponent<Fluky::TransformComponent>();
-			axeX += player.GetAxes()[1]*0.1;
-			axeY += player.GetAxes()[0]*0.1;
-			transf.RotateXY(1.f + axeX, 1.f + axeY);
+			if (i == 0) {
+				axeXP1 += player.GetAxes()[1] * 0.1;
+				axeYP1 += player.GetAxes()[0] * 0.1;
+				transf.RotateXY(1.f + axeXP1, 1.f + axeYP1);
+			}
+			else if (i == 1) {
+				axeXP2 += player.GetAxes()[1] * 0.1;
+				axeYP2 += player.GetAxes()[0] * 0.1;
+				transf.RotateXY(1.f + axeXP2, 1.f + axeYP2);
+			}
+			
 		}
 
 		//world.GetJoystickInput();
@@ -35,8 +51,11 @@ public:
 
 	}
 
-	float axeX = 0;
-	float axeY = 0;
+	float axeXP1 = 0;
+	float axeYP1 = 0;
+
+	float axeXP2 = 0;
+	float axeYP2 = 0;
 
 };
 
