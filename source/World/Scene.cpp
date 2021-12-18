95b565242b4dd787226c7aef5c48f5dbf6929348
LukasGL <lukas.gribbell@ug.uchile.cl>
2021-12-18 15:00:05 -0300

    Fixed startup with refactor

--- source/World/Scene.cpp
+++ source/World/Scene.cpp
@@ -13,8 +13,26 @@ namespace Fluky {
 
 	void Scene::StartUp() {
 		audioManager.StartUp();
-		for (auto i = gameObjectVector.begin(); i != gameObjectVector.end(); ++i) {
+		/*for (auto i = gameObjectVector.begin(); i != gameObjectVector.end(); ++i) {
 			i->StartUp(*this);
+		}*/
+
+		auto view = registry.view<BoxComponent>();
+
+		for (auto entity : view)
+		{
+			auto& box = view.get<BoxComponent>(entity);
+
+			box.Init();
+		}
+
+		auto view3 = registry.view<AudioComponent>();
+
+		for (auto entity : view3)
+		{
+			auto& audio = view3.get<AudioComponent>(entity);
+
+			audio.StartUp(*this);
 		}
 	}
 
